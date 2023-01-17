4bf571d17a56d7e223d4dd3a7aa7a05cf6d2fb91
Tyler Hatfield <hatfield.tyler@gmail.com>
2023-01-17 15:07:42 -0500

    created instance resource and created userdata.tpl

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "aws_vpc" "dev_vpc" {
-  cidr_block                = "10.123.0.0/16"
-  enable_dns_hostnames      = true
-  enable_dns_support        = true
-  
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
     Name = "Dev"
   }
 }
 
 resource "aws_subnet" "dev_public_subnet" {
-  vpc_id                        = aws_vpc.dev_vpc.id
-  cidr_block                    = "10.123.1.0/24"
-  map_public_ip_on_launch       = true
-  availability_zone             = "us-east-2a"
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-2a"
 
   tags = {
     Name = "Dev-public"
@@ -36,7 +36,56 @@ resource "aws_route_table" "dev-public-rt" {
 }
 
 resource "aws_route" "default_route" {
-  route_table_id = aws_route_table.dev-public-rt.id
+  route_table_id         = aws_route_table.dev-public-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.dev_internet_gateway.id
+  gateway_id             = aws_internet_gateway.dev_internet_gateway.id
+}
+
+resource "aws_route_table_association" "dev_public_association" {
+  subnet_id      = aws_subnet.dev_public_subnet.id
+  route_table_id = aws_route_table.dev-public-rt.id
+
+}
+
+resource "aws_security_group" "dev_sg" {
+  name        = "dev_sg"
+  description = "Dev Environment security group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.0.23/32"] #Update this ip to what your current IP is set at
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_key_pair" "dev_auth" {
+  key_name   = "devkey"
+  public_key = file("~/.ssh/DevEnvKey.pub") #This is where the key pair is located
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.dev_auth.id
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
+  subnet_id              = aws_subnet.dev_public_subnet.id
+  user_data = file("userdata.tpl")
+  
+  root_block_device {
+    volume_size = 10 #dont increase this because it may no longer be in the free tier
+  }
+
+  tags = {
+    Name = "Dev-Node"
+  }
 }
